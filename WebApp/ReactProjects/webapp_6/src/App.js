e18d4f4cc09cc41402d43b30ad2ecd08c9185fff
Ana <anadimoska06@gmail.com>
2023-06-07 14:13:53 +0100

    arrow keys

---
+++ WebApp/ReactProjects/webapp_6/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import GridMap from './components/GridMap';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>MazeMaster Control Panel</h1>
+      <GridMap />
+    </div>
+  );
+}
+
+export default App;
